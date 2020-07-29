32d06265bf8648f3acba0a3e41f7fbe0d3b5510c
FernandoCM18 <fernando.c.m@live.com>
2020-07-29 15:02:52 -0700

    docs agregado

---
+++ docs/precache-manifest.164198eefe3ffcb4c3f8e96740f6fed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b206a3b9c83de207ecc40d889514f83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84a08bb0a217ef099e2e",
+    "url": "/static/css/main.4bf9226a.chunk.css"
+  },
+  {
+    "revision": "dcf09d6ede3b15119fcb",
+    "url": "/static/js/2.a16041ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a16041ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a08bb0a217ef099e2e",
+    "url": "/static/js/main.48089795.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
